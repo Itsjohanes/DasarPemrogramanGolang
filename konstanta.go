b104e06ceb0cf2b1851dbd9867673e24cac4a489
itsjohanes <johannesap@upi.edu>
2025-01-01 08:38:02 +0700

    first commit

---
+++ konstanta.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+func main(){
+	const phi = 3.14
+	fmt.Println("Nilai phi:", phi)
+	const (
+		firstName = "John"
+		lastName = "Doe"
+		age = 25
+	)
+	fmt.Println("First Name:", firstName)
+	fmt.Println("Last Name:", lastName)
+	fmt.Println("Age:", age)
+}
---
+++ variable.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var firstName string = "John"
+	var lastName string = "Doe"
+	var age int = 25
+	var salary float64 = 3500.50
+	var isConfirmed bool = true
+	fmt.Println("First Name:", firstName)
+	fmt.Println("Last Name:", lastName)
+	fmt.Println("Age:", age)
+	fmt.Println("Salary:", salary)
+	fmt.Println("Is Confirmed:", isConfirmed)
+	
+}
